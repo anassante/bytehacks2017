7e38c979f2d7cd142c4845d0388998ae6d8e54d9
aassante <aassante@hawaii.edu>
2017-06-25 11:28:52 -0400

    create serial user interface

--- main.ino
+++ main.ino
@@ -2,23 +2,18 @@ Servo rservo;  // create servo object to control a servo
                 // a maximum of eight servo objects can be created 
 Servo lservo;
 
-const int LEFT = 0;
-const int RIGHT = 1;
-const int FORWARD = 2;
-const int BACKWARD = 3;
+// WASD controls, no backward direction so no S defined
+
+const int LEFT = 97;     // A
+const int RIGHT = 100;   // D
+const int FORWARD = 119; // W
+
+const int HELP = 104;    // H
 
 int pos = 0;    // variable to store the servo position 
-int dir = FORWARD;
-int count = 0;
-int step = 0;  // index for route
+int dir = 2;
 
-int route[] = {FORWARD, RIGHT, FORWARD, LEFT, LEFT, FORWARD};
-
-// static char inData[51]; // Allocate some space for the string
-// static char inChar=-1; // Where to store the character read
-// static int charpos= 0; // Index into array; where to store the character
-
-// int incoming = 0;
+int incomingByte = 0;
 
 void setup() 
 { 
@@ -39,50 +34,60 @@ void buzz(Servo svo)
     svo.write(pos);              // tell servo to go to position in variable 'pos' 
     delay(15);                       // waits 15ms for the servo to reach the position 
   }
-  Serial.println("buzz");
 }
 
 void double_buzz(Servo svo1, Servo svo2)
 {
-    for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
-    {                                  // in steps of 1 degree 
-        svo1.write(pos);              // tell servo to go to position in variable 'pos' 
-        svo2.write(pos);
-        delay(500);                    
-    } 
-    for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
-    {                                
-        svo1.write(pos);              // tell servo to go to position in variable 'pos' 
-        svo2.write(pos);
-        delay(500);   
-    }
-    Serial.println("double buzz");
+  for(pos = 0; pos < 180; pos += 1)  // goes from 0 degrees to 180 degrees 
+  {                                  // in steps of 1 degree 
+    svo1.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position 
+    svo2.write(pos);
+  } 
+  for(pos = 180; pos>=1; pos-=1)     // goes from 180 degrees to 0 degrees 
+  {                                
+    svo1.write(pos);              // tell servo to go to position in variable 'pos' 
+    delay(15);                       // waits 15ms for the servo to reach the position
+    svo2.write(pos);
+  }
 }
 
-//// Different vibration pattern inserted in between different direction calls
-//// to make it clearer that direction has changed
-//void transition_alert(Servo svo1, Servo svo2)
+void help()
+{
+    Serial.println("Welcome to Blind Assassin!");
+    Serial.println("Handler: Move your teammate using the keyboard.");
+    Serial.println("Left - 'a' Forward - 'w' Right - 'd'");
+    Serial.println("Assassin: Move left when left motor buzzes.");
+    Serial.println("Assassin: Move right when right motor buzzes.");
+    Serial.println("Assassin: Move forward when both motors buzz.");
+}
 
 void loop() 
 {
-    Serial.println("test");
+    if (Serial.available() > 0) {
+                // read the incoming byte:
+                incomingByte = Serial.read();
+                
+                // else if statements did not work here for reasons for which there was no time to debug
+                if (incomingByte == FORWARD)
+                {
+                    double_buzz(rservo, lservo);
+                }
+                if (incomingByte == LEFT)
+                {
+                    buzz(lservo);
+                } 
+                if (incomingByte == RIGHT)
+                {
+                    buzz(rservo);
+                } 
+                if (incomingByte == HELP)
+                {
+                    help();
+                }
 
-    if (count = 200)
-    {
-        dir = route[step];
-        step++;
-        count = 0;
-    }
-    count++;
-    if (dir == RIGHT)
-    {
-        buzz(rservo);
-    } else if (dir == LEFT)
-    {
-        buzz(lservo);
-    } else if (dir == FORWARD)
-    {
-        double_buzz(rservo, lservo);
-    }
-    
+                // say what you got:
+                Serial.print("I received: ");
+                Serial.println(incomingByte, DEC);
+        }
 }
